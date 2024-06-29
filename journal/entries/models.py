61534d05311c73db59c6e4246ae9eb6e23866ec0
katene30 <katenejack@gmail.com>
2024-06-29 23:26:24 +1200

    update entry model and register snippet

--- journal/entries/models.py
+++ journal/entries/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from wagtail.fields import RichTextField
+from wagtail.snippets.models import register_snippet
 
+@register_snippet
 class JournalEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
@@ -10,12 +12,12 @@ class JournalEntry(models.Model):
     depression_level = models.IntegerField()
     anxiety_level = models.IntegerField()
     stress_level = models.IntegerField()
-    sleep_hours = models.DecimalField(max_digits=4, decimal_places=1)
-    sleep_quality = models.IntegerField()
+    sleep_hours = models.DecimalField(max_digits=4, blank=True)
+    sleep_quality = models.IntegerField(blank=True)
     energy_level = models.IntegerField()
-    social_interactions_quality = models.IntegerField()
-    productivity_level = models.IntegerField()
-    diet_quality = models.IntegerField()
+    social_interactions_quality = models.IntegerField(blank=True)
+    productivity_level = models.IntegerField(blank=True)
+    diet_quality = models.IntegerField(blank=True)
     alcohol_caffeine_consumption = models.CharField(max_length=255, blank=True)
     self_care_effectiveness = models.IntegerField()
     significant_events = models.TextField(blank=True)
@@ -23,3 +25,6 @@ class JournalEntry(models.Model):
 
     def __str__(self):
         return f"Entry for {self.user} on {self.date}"
+    
+    class Meta:
+        verbose_name_plural = "journal entries"
