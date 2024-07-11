bad8082952eb4485682881653af7f4eb04694581
katene30 <katenejack@gmail.com>
2024-07-12 09:48:32 +1200

    remove comment

--- journal/entries/templatetags/entries_tags.py
+++ journal/entries/templatetags/entries_tags.py
@@ -7,7 +7,8 @@ register = template.Library()
 
 @register.inclusion_tag('entries/entry_list.html', takes_context=True)
 def entries(context):
+    entries = JournalEntry.objects.order_by('date')
     return {
-        'entries': JournalEntry.objects.all(),
+        'entries': entries,
         'request': context['request'],
     }
