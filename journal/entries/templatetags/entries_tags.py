4e13534d7bfa9c35557260a1d84c2df17f11d5b8
katene30 <katenejack@gmail.com>
2024-08-21 15:40:01 +1200

    list entries by most recent

--- journal/entries/templatetags/entries_tags.py
+++ journal/entries/templatetags/entries_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.inclusion_tag('entries/entry_list.html', takes_context=True)
 def entries(context):
-    entries = JournalEntry.objects.order_by('date')
+    entries = JournalEntry.objects.order_by('-date')
     return {
         'entries': entries,
         'request': context['request'],
