6580051a1a2674823cd229c71cdbc8a38cc22c3b
katene30 <katenejack@gmail.com>
2025-06-23 20:24:10 +1200

    update allowed_hosts

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['myjournal-4b9a415f0dd2.herokuapp.com']
+ALLOWED_HOSTS = ['myjournal-4b9a415f0dd2.herokuapp.com', "localhost", "127.0.0.1"]
 
 
 # Application definition
