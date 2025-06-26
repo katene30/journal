77382dc3bb253d74a4bf7bd19b80b5eff3a7c9ac
katene30 <katenejack@gmail.com>
2025-06-26 18:28:36 +1200

    add favicon

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -158,3 +158,5 @@ if os.getenv('DATABASE_URL'):
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
