657fad9f47e08739b1bd70cd8f76db4a192fab80
katene30 <katenejack@gmail.com>
2026-06-12 16:52:56 +1200

    debug: temporarily enable DEBUG to diagnose Render 500 error

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+# TODO: Set back to 'False' after debugging Render 500 error
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
